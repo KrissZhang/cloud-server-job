c1a1db71a1f31db5f2f301b3702d0a7ce5fe1755
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2021-11-09 10:59:28 +0800

    提交

---
+++ cloud-server-job-server/src/main/java/com/self/cloudserver/CloudServerJobServerApplication.java
@@ -0,0 +1,13 @@
+package com.self.cloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerJobServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerJobServerApplication.class, args);
+    }
+
+}
