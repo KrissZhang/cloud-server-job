002847f94e87c7fb472d9a32b97d7ee783700fd6
ZhangPeng <36990035+KrissZhang@users.noreply.github.com>
2021-11-09 16:23:06 +0800

    测试Http任务

--- cloud-server-job-server/src/main/java/com/self/cloudserver/handler/TestJobHandler.java
+++ cloud-server-job-server/src/main/java/com/self/cloudserver/handler/TestJobHandler.java
@@ -1,11 +1,27 @@
 package com.self.cloudserver.handler;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class TestJobHandler {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @XxlJob(value = "testJobHandler")
     public void testJobHandler() throws Exception {
@@ -16,6 +32,27 @@ public class TestJobHandler {
         XxlJobHelper.log("===TestJobHandler=== testJobHandler");
 
         //执行返回结果可以在执行日志中查看到
+        XxlJobHelper.handleSuccess("执行成功");
+    }
+
+    @XxlJob(value = "testHttpJobHandler")
+    public void testHttpJobHandler() throws Exception {
+        String param = XxlJobHelper.getJobParam();
+        JSONObject object = JSON.parseObject(param);
+        String req = Optional.ofNullable(object.getString("req")).orElse("");
+
+        Map<String, String> map = new HashMap<>();
+        req += ("|" + DATE_TIME_FORMATTER.format(LocalDateTime.now()));
+        map.put("req", req);
+        String url = "http://127.0.0.1:9000/rpc/cloud/server/two/test?req={req}";
+        ResponseEntity<JSONObject> responseEntity = restTemplate.getForEntity(url, JSONObject.class, map);
+
+        JSONObject result = responseEntity.getBody();
+        String data = Optional.ofNullable(result.getString("data")).orElse("");
+
+        XxlJobHelper.log("===testHttpJobHandler=== 获取返回结果：" + data);
+        System.out.println("===testHttpJobHandler=== 获取返回结果：" + data);
+
         XxlJobHelper.handleSuccess("执行成功");
     }
 
